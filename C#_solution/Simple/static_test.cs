cc4b72b399607811b34fa7d6d528e995b8931548
algoscienceacademy <sshahrearhossain@gmail.com>
2024-10-14 13:36:52 +0600

    update v01

---
+++ C#_solution/Simple/static_test.cs
@@ -0,0 +1,43 @@
+﻿/*
+using System;
+namespace cs
+{
+    class Person
+
+    {
+        static string name;
+        static int age;
+
+
+
+        static public void getdata()
+        {
+            Console.WriteLine("enter name");
+            name = (Console.ReadLine());
+            Console.WriteLine("enter ");
+            age = Convert.ToInt32(Console.ReadLine());
+        }
+
+        static public void display()
+        {
+            Console.WriteLine($" name is {name}");
+            Console.WriteLine($" name is {age}");
+        }
+
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+
+            Person.getdata();
+            Person.display();
+
+
+        }
+
+    }
+
+}*/
